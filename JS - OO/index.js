6a63172744feb8984da3f7cc20d05498592a5754
Mayke Wazouski <mayke.mx@hotmail.com>
2021-08-22 12:45:27 -0300

    Adicionado comentários

--- JS - OO/index.js
+++ JS - OO/index.js
@@ -5,16 +5,27 @@ class Cliente {
 
 class ContaCorrente{
     agencia;
-    saldo;
-
+    #saldo = 0;
+    
+    //metodos da classe
     sacar(valor){
-        if (this.saldo >= valor){
-            this.saldo -= valor;
+        if (this.#saldo >= valor){
+            this.#saldo -= valor;
         }
     }
-}
+    //metodos da classe
+        depositar(valor){
+            if(valor> 0){
+                this.#saldo += valor;
+               console.log(this.#saldo) 
+            }
+        } 
+         
+    }
 
+/*================================================*/
 
+//novas instancias com as chamadas de atributos da classe clinte
 const cliente1 = new Cliente();
 cliente1.nome = "Mayke";
 cliente1.cpf = "11122233300";
@@ -23,14 +34,15 @@ const cliente2 = new Cliente()
 cliente2.nome = "Alice";
 cliente2.cpf  = "11122233325";
 
+/*================================================*/
+
+// novas instacnias com a classe de Conta Corrente
 const contaCorrenteRicardo = new ContaCorrente();
- contaCorrenteRicardo.saldo = 100;
  contaCorrenteRicardo.agencia = 1001;
 
- console.log("");
- console.log(contaCorrenteRicardo.saldo);
+ 
+ contaCorrenteRicardo.depositar(100);
  contaCorrenteRicardo.sacar(50);
- console.log(contaCorrenteRicardo.saldo);
 
-console.log(cliente1);
-console.log(cliente2);
+ console.log(contaCorrenteRicardo);
+
